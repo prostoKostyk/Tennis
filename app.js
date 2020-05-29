7d7e97c7d6af806c8154e08becb64862192978cc
prostoKostyk <osockin.kos@yandex.ru>
2020-05-29 10:46:33 +0300

    -

--- app.js
+++ app.js
@@ -83,23 +83,31 @@ var Ball = /** @class */ (function () {
         // определение направления
         // проверка удара об игрока
         if (this.posY + this.stepY >= player.posY && (this.posX > player.posX - 5 && this.posX < player.posX + player.width)) {
-            // this.directionX = this.directionX;
+            this.playerTouchX = (ball.posX - player.posX - 55) / 10;
             this.directionY = false;
+            if (this.playerTouchX >= 0) {
+                this.directionX = true;
+            }
+            else {
+                this.directionX = false;
+            }
+            // изменение шага
+            if (this.playerTouchX < 0) {
+                this.playerTouchX *= -1;
+            }
+            this.stepX = (this.playerTouchX + 1) * 1.75;
         }
         // проверка удара об потолок
         if (this.posY + this.stepY <= 10) {
-            // this.directionX = !this.directionX;
             this.directionY = true;
         }
         // проверка удара об правую стену
         if (this.posX + this.stepX >= field.width - 10) {
             this.directionX = false;
-            // this.directionY = !this.directionY;
         }
         // проверка удара об левую стену
         if (this.posX - this.stepX <= 10) {
             this.directionX = true;
-            // this.directionY = !this.directionY;
         }
         // проверка на вылет шара вниз
         if (this.posY > field.height - 10) {
--- app.ts
+++ app.ts
@@ -48,6 +48,7 @@ class Ball {
     stepY = 5;
     directionX = true; // true - право, false - лево
     directionY = true; // true - вниз, false - вверх
+    playerTouchX;
     constructor(posX: number, posY: number) {
         this.posX = posX;
         this.posY = posY;
@@ -81,23 +82,30 @@ class Ball {
         // определение направления
         // проверка удара об игрока
         if (this.posY + this.stepY >= player.posY && (this.posX > player.posX - 5 && this.posX < player.posX + player.width)) {
-            // this.directionX = this.directionX;
+            this.playerTouchX = (ball.posX - player.posX - 55) / 10;
             this.directionY = false;
+            if (this.playerTouchX >= 0) {
+                this.directionX = true;
+            } else {
+                this.directionX = false;
+            }
+            // изменение шага
+            if (this.playerTouchX < 0) {
+                this.playerTouchX *= -1;
+            }
+            this.stepX = (this.playerTouchX + 1) * 1.75;
         }
         // проверка удара об потолок
         if (this.posY + this.stepY <= 10) {
-            // this.directionX = !this.directionX;
             this.directionY = true;
         }
         // проверка удара об правую стену
         if (this.posX + this.stepX >= field.width - 10) {
             this.directionX = false;
-            // this.directionY = !this.directionY;
         }
         // проверка удара об левую стену
         if (this.posX - this.stepX <= 10) {
             this.directionX = true;
-            // this.directionY = !this.directionY;
         }
         // проверка на вылет шара вниз
         if (this.posY > field.height - 10) {
